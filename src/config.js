2c7cf165227ef74bfbcfa5600eb368e4b2f30654
abhaysood1793 <abhayasood99@gmail.com>
2023-01-15 23:27:59 +0530

    sunday complete

--- src/config.js
+++ src/config.js
@@ -5,14 +5,14 @@ export default{
     //main section on the top
     name:"Abhay Sood.",
     intro_one_liner:"I am an android and web developer.",
-    short_main_intro:"Welcome to my portfolio website! I am an android and web developer, dedicated to honing my skills and continuously seeking new opportunities to improve. My work, showcased on this website, browse my portfolio to see the solutions I can offer for your business or organization",
+    short_main_intro:"Welcome to my portfolio website! I am an android and web developer, dedicated to honing my skills and continuously seeking new opportunities to improve. My work, showcased on this website, browse my portfolio to see the solutions I can offer for your business or organization.",
 
     //about me section
     p1:"I am a computer science student at National Institute of Technology, Hamirpur, with a passion for developing innovative and efficient software solutions. My experience in android and web development, combined with my skills in freelancing and Discord server management, make me a well-rounded developer capable of handling a wide range of projects.",
     p2:"I have worked on several android and web projects, including an admin panel for an edtech app, an android application for shortlisting clients based on location, and a pdf reader app. These projects have allowed me to gain valuable experience in developing secure and user-friendly software, as well as in integrating various technologies such as AWS and Firebase.",
     p3:"In addition to my technical skills, I am a dedicated and hardworking individual who is always eager to learn and improve. As a class representative at NIT Hamirpur and a volunteer for the App Team, I have developed strong leadership and teamwork skills.",
     p4:"I am looking for opportunities to showcase my skills and continue developing my abilities as a software developer. I am excited to take on new challenges and to contribute to the development of cutting-edge technologies.",
-    skills:["js","react","android","java","cpp","special"],
+    skills:["Java","Android","Firebase","JavaScript","ReactJS","C++"],
 
     //social links
     github:"https://github.com/humaneBicycle",
