6af62f98e271abc8410a491fe2021d3cdb7a7a72
abhaysood1793 <abhayasood99@gmail.com>
2023-01-24 21:39:35 +0530

    'visitors'

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import config from "../config";
 
 export default function Footer() {
+  let [visitors, setVisitors] = React.useState(undefined);
+  useEffect(() => {
+    getVisitors();
+  }, []);
+  let getVisitors = async () => {
+    let res = await fetch("https://api.countapi.xyz/hit/abhayasood.xyz/visits");
+    let data = await res.json();
+    setVisitors(data.value);
+    console.log(data);
+  };
   return (
-    <div className="footer">
-      <a href={config.repo_link} target="_blank" rel="noopener noreferrer" className="footer_container">
-        Build by {config.name}
-      </a>
-    </div>
+    <>
+      <div className="footer">
+        <div className="total_visitors">
+          Total visitors:
+          {visitors !== undefined ? visitors : <></>}
+        </div>
+        <div className="build_link">
+          <a
+            href={config.repo_link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="footer_container"
+          >
+            Build by {config.name}
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
