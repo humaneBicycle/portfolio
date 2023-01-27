41821b451d6951d214ad63f783dc1523a2b5e4cd
abhaysood1793 <abhayasood99@gmail.com>
2023-01-27 15:04:46 +0530

    minor

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export default{
     short_main_intro:"Welcome to my portfolio website! I am an android and web developer, dedicated to honing my skills and continuously seeking new opportunities to improve. My work, showcased on this website, browse my portfolio to see the solutions I can offer for your business or organization.",
 
     //about me section
-    p1:"I am a computer science student at National Institute of Technology, Hamirpur, with a passion for developing innovative and efficient software solutions. My experience in android and web development, combined with my skills in freelancing and Discord server management, make me a well-rounded developer capable of handling a wide range of projects.",
+    p1:"I am a computer science student at National Institute of Technology, Hamirpur, with a passion for developing innovative and efficient software solutions. My experience in android and web development, combined with my skills in freelancing, make me a well-rounded developer capable of handling a wide range of projects.",
     p2:"I have worked on several android and web projects, including an admin panel for an edtech app, an android application for shortlisting clients based on location, and a pdf reader app. These projects have allowed me to gain valuable experience in developing secure and user-friendly software, as well as in integrating various technologies such as AWS and Firebase.",
     p3:"In addition to my technical skills, I am a dedicated and hardworking individual who is always eager to learn and improve. As a class representative at NIT Hamirpur and a volunteer for the App Team, I have developed strong leadership and teamwork skills.",
     p4:"I am looking for opportunities to showcase my skills and continue developing my abilities as a software developer. I am excited to take on new challenges and to contribute to the development of cutting-edge technologies.",
@@ -48,7 +48,7 @@ export default{
             title:"Admin Panel: Full Stack Edtech App",
             description:"An admin panel in reactJS which handles CRUD for a frontend android application which is deployed on Netlify and the backend as nodeJS server deployed on Heroku. The Video lessons for the app are uploaded to AWS S3 and are converted to various qualities using AWS Mediaconvert. The security layer is implemented using JWT Tokens. ",
             image_url:"https://ik.imagekit.io/jpnn4qofy/Portfolio/edtech-compressed.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1674496287969",
-            skills:["ReactJS","Firebase","AWS","Vercel","Netlify"],
+            skills:["ReactJS","Firebase","AWS","Vercel","Netlify","RazorPay Api","JWT Tokens","HLS Streaming"],
             github_link:"",
             live_link:"https://quasar-edtech-admin.netlify.app/"
 
